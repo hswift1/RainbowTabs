28db9f4d35d8f2af97648d03b357a899567946be
Soft Dev <softdev@robotics-mba-07.local>
2019-04-02 14:42:27 -0400

    Initial Commit

---
+++ RainbowTabs/RedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Soft Dev on 3/28/19.
+//  Copyright © 2019 Lily Guo. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tabBarItem.badgeValue = nil
+    }
+
+
+}
+
